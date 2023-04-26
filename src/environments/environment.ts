b26f18a53bef00f360aa7fbee958320652076a94
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2023-04-26 02:13:06 +0100

    Using AngularFireAuth and AngularFirestore methods

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyC8R63TG7NRcPBnr10hAhJ6PKcaA6jgjgI",
+        authDomain: "fireauth-c6306.firebaseapp.com",
+        projectId: "fireauth-c6306",
+        storageBucket: "fireauth-c6306.appspot.com",
+        messagingSenderId: "448011171106",
+        appId: "1:448011171106:web:46b42edf47a85dd107617b"
+    }
+};
